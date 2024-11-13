c3a752287e28b7dbe5680d8105fb2b7f38dcdedc
Heet Kumbharana <159053359+HEETKUMBHARANA2369@users.noreply.github.com>
2024-11-13 11:58:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -91,7 +91,7 @@ def threaded_chat():
     engine.setProperty('voice', voices[0].id)
 
     url = "https://lokiai.vercel.app/api/v1/chat/completions"
-    api_key = "USE_YOUR_API_kEY"
+    api_key = "HEET-NI-CHEESEWADI-API-KEY"
     #contact Parth sadaria for your api, mentioned below in the README.md
 
     # global chat_history
